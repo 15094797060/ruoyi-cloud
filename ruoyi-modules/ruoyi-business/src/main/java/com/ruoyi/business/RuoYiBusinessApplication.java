27edc70145af56c6c0dcee21f2f930a0f9440b60
15094797060 <lihao_0538@163.com>
2021-04-08 10:16:49 +0800

    首次提交

---
+++ ruoyi-modules/ruoyi-business/src/main/java/com/ruoyi/business/RuoYiBusinessApplication.java
@@ -0,0 +1,32 @@
+package com.ruoyi.business;
+
+import com.ruoyi.common.security.annotation.EnableCustomConfig;
+import com.ruoyi.common.security.annotation.EnableRyFeignClients;
+import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * 业务模块
+ *
+ * @author 李浩
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableRyFeignClients
+@SpringCloudApplication
+public class RuoYiBusinessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuoYiBusinessApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  业务模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
